47cfa9a1a71cbaa35163ee157216b4cf0d7de838
mirkoperamas <you@example.com1421133@utp.edu.pe>
2022-03-29 18:50:08 -0500

    act

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,4 @@
 import { Carousel } from "./components/Carousel/Carousel";
-import { Header } from "../../components/Header/Header";
 import classes from "./home.module.css";
 
 export const HomeView = () => {
